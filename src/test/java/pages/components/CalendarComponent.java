d6aec86090b8357b3813f4c5c73c13a7cbbde05c
Andrey Matrosov <amtrsv@yandex.ru>
2023-11-10 00:51:59 +0300

    Added changes for PageObject tests

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -7,11 +7,8 @@ import static com.codeborne.selenide.Selenide.$;
 public class CalendarComponent {
     public SelenideElement yearSelect = $(".react-datepicker__year-select");
     public SelenideElement monthSelect = $(".react-datepicker__month-select");
-    public SelenideElement birthInput = $("#dateOfBirthInput");
-
 
     public void setDateOfBirth(String day, String month, String year) {
-        birthInput.click();
         yearSelect.selectOption(year);
         monthSelect.selectOption(month);
         $(".react-datepicker__day.react-datepicker__day--0"+day+":not(.react-datepicker__day--outside-month)").click();
